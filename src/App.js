98586ecdd56c440985d95c07d5c026ddcf5d3cd9
Ashwin <ashwinsrk77@gmail.com>
2022-01-26 16:30:23 +0530

    Navbar added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import './App.css';
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Footer from "./components/footer";
+import Navbar from "./components/navbar";
+import LandingPage from "./components/Landing_page";
 
 function App() {
   return (
-    <div>
-      Hello BlocSoc_IITR
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<LandingPage />}></Route>
+      </Routes>
+      <Footer />
+    </Router>
   );
 }
 
