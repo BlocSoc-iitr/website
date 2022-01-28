6be709df9902ce32de6421fe6c0d9ab402c8ba98
Akshat Maheshwari <0.akshat.maheshwari@gmail.com>
2022-01-29 01:04:34 +0530

    footer

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
         <Route path="/resources" element={<Resources />}></Route>
         <Route path="/" element={<LandingPage />}></Route>
       </Routes>
-      <Footer />  
+      {/* <Footer />   */}
     </Router>
   );
 }
