c3aa47f0ba360127183964b88dc1f2f12c42010b
Ashwin <ashwinsrk77@gmail.com>
2022-02-03 11:49:46 +0530

    Alternate EventCard Fixed

--- src/components/eventCard/index.js
+++ src/components/eventCard/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import './eventCard.css';
 
-function EventCard() {
+function EventCard(props) {
   return (
-      <div className='event_card'>
+      <div className='event_card' style={{"flexDirection":props.flexDirection}}>
           <img src="" className='event_img' />
           <div className='event_des'>
               <p className='event_title'>Name of Event</p>
