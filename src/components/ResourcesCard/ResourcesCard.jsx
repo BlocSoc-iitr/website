bced7f6d258e4b0d8f0b868b2dc4586318725dde
Akshat Maheshwari <0.akshat.maheshwari@gmail.com>
2022-02-13 22:44:22 +0530

    resources padding fix and hover effect on card

--- src/components/ResourcesCard/ResourcesCard.jsx
+++ src/components/ResourcesCard/ResourcesCard.jsx
@@ -5,12 +5,15 @@ import image from "../../assets/resources-card.svg";
 function ResourcesCard() {
   return <div>
       <div className="resources_card">
-            <div className="blogs_grey_area">
-            <img className="resource_card_img" src={image} alt="" />
-            </div>
-                <div className="blogs_title">
-                Title
-                </div>
+        <div className="blogs_grey_area">
+        <div className="resource_card_img_hover">
+          Here we'll have description of blog/ video on hover. Also on click we'll navigate to new page.
+        </div>
+          <img className="resource_card_img" src={image} alt="" />
+        </div>
+        <div className="blogs_title">
+          Title
+        </div>
       </div>
       </div>
 }
